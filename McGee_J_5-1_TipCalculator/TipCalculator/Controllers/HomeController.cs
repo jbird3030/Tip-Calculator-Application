926bbd8e392a3981cf74c7f14027d15f462541cd
jbird3030 <79331033+jbird3030@users.noreply.github.com>
2022-04-05 14:04:19 -0400

    Tip Calculator Application

---
+++ McGee_J_5-1_TipCalculator/TipCalculator/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+           ViewBag.FifteenTip = 0;
+            ViewBag.TwentyTip = 0;
+            ViewBag.TwentyFiveTip = 0;
+           return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index (Calculator model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FifteenTip = model.FifteenTip();
+            }
+            else
+            {
+                model.MealCost = 0;
+            }
+            if (ModelState.IsValid)
+            {
+                ViewBag.TwentyTip = model.TwentyTip();
+            }
+            else
+            {
+                model.MealCost = 0;
+            }
+            if (ModelState.IsValid)
+            {
+                ViewBag.TwentyFiveTip = model.TwentyFiveTip();
+            }
+            else
+            {
+                model.MealCost = 0;
+            }
+
+            ViewBag.MealCost = model.MealCost;
+            ViewBag.TwentyFiveTip = model.TwentyFiveTip();
+            ViewBag.TwentyTip = model.TwentyTip();
+            ViewBag.FifteenTip = model.FifteenTip();
+            return View();
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ McGee_J_5-1_TipCalculator/TipCalculator/Models/TipCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculator.Models
+{
+    public class Calculator
+    {
+        [Required(ErrorMessage = "Please enter a meal cost.")]
+        [Range(.01, 9999999999999999999, ErrorMessage = "Please enter a valid meal cost.")]
+        public decimal? MealCost { get; set; }
+
+        public decimal? FifteenTip()
+        {
+            decimal? FifteenTip = 0;
+            decimal? FifteenPercent = (decimal?).15;
+            FifteenTip = MealCost * FifteenPercent;
+            return FifteenTip;
+        }
+
+        public decimal? TwentyTip()
+        {
+            decimal? TwentyTip = 0;
+            decimal? TwentyPercent = (decimal?).20;
+            TwentyTip = MealCost * TwentyPercent;
+            return TwentyTip;
+        }
+
+
+        public decimal? TwentyFiveTip()
+        {
+            decimal? TwentyFiveTip = 0;
+            decimal? TwentyFivePercent = (decimal?).25;
+            TwentyFiveTip = MealCost * TwentyFivePercent;
+            return TwentyFiveTip;
+        }
+
+
+
+
+
+
+    }
+}
